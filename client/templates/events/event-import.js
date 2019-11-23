2e65b54a1a95444ed5e0bda593b62b11c22fac13
EdUTroN <edtronco@gmail.com>
2019-11-23 03:10:00 -0300

    Correción Sesión
    - Evita error por import sin sesion

--- client/templates/events/event-import.js
+++ client/templates/events/event-import.js
@@ -6,58 +6,58 @@ var listRenderHold = LaunchScreen.hold();
 listFadeInHold = null;
 
 Template.eventImport.rendered = function () {
-  if (firstRender) {
-    listFadeInHold = LaunchScreen.hold();
-    // Handle for launch screen defined in app-body.js
-    listRenderHold.release();
-    firstRender = false;
-  }
+   if (firstRender) {
+      listFadeInHold = LaunchScreen.hold();
+      // Handle for launch screen defined in app-body.js
+      listRenderHold.release();
+      firstRender = false;
+   }
 }
 
 Template.eventImport.helpers({
-  evento: function () {
-    var e = Session.get('EventoSeleccionado');
-    if (e) return e;
-  },
-  messages: function () {
-    var messages = Session.get('ImportMessages');
-    return messages;
-  }
+   evento: function () {
+      var e = Session.get('EventoSeleccionado');
+      if (e) return e;
+   },
+   messages: function () {
+      var messages = Session.get('ImportMessages');
+      return messages;
+   }
 });
 
 Template.eventImport.events({
-  'click #btn-import': function () {
-    var messages;
+   'click #btn-import': function () {
+      if (!Meteor.user()) Router.go("/");
+      var messages;
 
-    var eventSelected = Session.get('EventoSeleccionado');
-    
-    var totalImported = 0;
+      var eventSelected = Session.get('EventoSeleccionado');
 
-    var text2Import = $('#ruts-2-import').val().trim();
+      var totalImported = 0;
 
-    if (text2Import.length == 0) {
-      messages = {
-        danger: []
-      };
-      messages.danger.push({
-        item: 'Se requieren datos para importar'
-      });
-      Session.set('ImportMessages', messages);
-      return;
-    }
-    var entradas = text2Import.split(/\r\n|\n|\r/);
-    var fechaEvento;
+      var text2Import = $('#ruts-2-import').val().trim();
 
-    $('#ruts-2-import').val('');
-    
-    Meteor.call("LogHT", "IMPORTAR_LISTA(" + entradas.length +"-REGS)", true);
-
-    Meteor.call('processListImport', entradas, eventSelected._id, function (err, data) {
-      if (err) {
-      } else {
-        Session.set('ImportMessages', data);
-        $('#ruts-2-import').val(data.wrongRuts);
+      if (text2Import.length == 0) {
+         messages = {
+            danger: []
+         };
+         messages.danger.push({
+            item: 'Se requieren datos para importar'
+         });
+         Session.set('ImportMessages', messages);
+         return;
       }
-    });
-  }
+      var entradas = text2Import.split(/\r\n|\n|\r/);
+      var fechaEvento;
+
+      $('#ruts-2-import').val('');
+
+      Meteor.call("LogHT", "IMPORTAR_LISTA(" + entradas.length + "-REGS)", true);
+
+      Meteor.call('processListImport', entradas, eventSelected._id, function (err, data) {
+         if (err) {} else {
+            Session.set('ImportMessages', data);
+            $('#ruts-2-import').val(data.wrongRuts);
+         }
+      });
+   }
 });
